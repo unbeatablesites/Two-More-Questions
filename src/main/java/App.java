3d86defdec15a9d8669df13e6de46491fb251fa3
Frank Uzoka <Frankuzoka@hotmail.com>
2020-02-16 13:03:00 -0500

    Doing logic for inside

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class App {
 
     public static void main(String[] args) {
@@ -15,6 +17,15 @@ public class App {
             Thread.sleep(2000);
         } catch (InterruptedException e) {
             e.printStackTrace();
+        }
+
+        System.out.println("Question 1) Does it stay inside or outside or both?");
+
+        Scanner input = new Scanner(System.in);
+        Scanner input2 = new Scanner(System.in);
+
+        if(input.equals("outside") && input2.equals("alive")){
+            System.out.println("");
         }
 
 
