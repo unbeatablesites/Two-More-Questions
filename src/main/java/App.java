c393cfdd606332263123225635e95c7effcbce76
Frank Uzoka <Frankuzoka@hotmail.com>
2020-02-16 12:56:00 -0500

    Add time delay to for next question

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,17 +1,21 @@
 public class App {
 
     public static void main(String[] args) {
-        doLogic();
+        doLogicForInside();
     }
 
 
-    public static void doLogic(){
+    public static void doLogicForInside(){
 
         System.out.println("TWO MORE QUESTIONS, BABY!\n" +
                 "\n" +
                 "Think of something and I'll try to guess it!");
 
-
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
 
 
 
