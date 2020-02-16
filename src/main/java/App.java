82a2dd0672137c8c114d6e18ba2de1803eade6f2
Frank Uzoka <Frankuzoka@hotmail.com>
2020-02-16 13:18:02 -0500

    Doing logic for inside

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -4,6 +4,8 @@ public class App {
 
     public static void main(String[] args) {
         doLogicForInside();
+        doLogicForOutside();
+        doLogicForBoth();
     }
 
 
@@ -25,8 +27,7 @@ public class App {
 
         String input = scan.nextLine().toLowerCase();
 
-
-        System.out.println("Question 2) Is it a living thing?");
+        System.out.println("Question 2) Is it a living thing? yes or no?");
 
         String input2 = scan.nextLine().toLowerCase();
 
@@ -34,8 +35,63 @@ public class App {
             System.out.println("Then what else could you be thinking of besides a python?!?\n");
         }
 
+    }
 
+    public static void doLogicForOutside(){
+
+        System.out.println("TWO MORE QUESTIONS, BABY!\n" +
+                "\n" +
+                "Think of something and I'll try to guess it!");
+
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("Question 1) Does it stay inside or outside or both?");
+
+        Scanner scan = new Scanner(System.in);
+
+        String input = scan.nextLine().toLowerCase();
+
+        System.out.println("Question 2) Is it a living thing? yes or no?");
+
+        String input2 = scan.nextLine().toLowerCase();
+
+        if(input.equals("outside") && input2.equals("yes")){
+            System.out.println("Then what else could you be thinking of besides a python?!?\n");
+        }
 
     }
+
+    public static void doLogicForBoth(){
+
+        System.out.println("TWO MORE QUESTIONS, BABY!\n" +
+                "\n" +
+                "Think of something and I'll try to guess it!");
+
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("Question 1) Does it stay inside or outside or both?");
+
+        Scanner scan = new Scanner(System.in);
+
+        String input = scan.nextLine().toLowerCase();
+
+        System.out.println("Question 2) Is it a living thing? yes or no?");
+
+        String input2 = scan.nextLine().toLowerCase();
+
+        if(input.equals("inside") && input2.equals("yes")){
+            System.out.println("Then what else could you be thinking of besides a python?!?\n");
+        }
+
+    }
+
 
 }
