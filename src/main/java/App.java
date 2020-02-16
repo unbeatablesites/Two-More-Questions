13f4a017283e99022259b6e101e03d4bdb0f86c9
Frank Uzoka <Frankuzoka@hotmail.com>
2020-02-16 12:49:13 -0500

    Add Main method

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,9 +1,20 @@
 public class App {
 
     public static void main(String[] args) {
-
+        doLogic();
     }
 
 
+    public static void doLogic(){
+
+        System.out.println("TWO MORE QUESTIONS, BABY!\n" +
+                "\n" +
+                "Think of something and I'll try to guess it!");
+
+
+
+
+
+    }
 
 }
