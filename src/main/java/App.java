8c0c691a21a92c3ab9188eb3ed8780ab27693a42
Frank Uzoka <Frankuzoka@hotmail.com>
2020-02-16 13:05:10 -0500

    Doing logic for inside

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -22,10 +22,19 @@ public class App {
         System.out.println("Question 1) Does it stay inside or outside or both?");
 
         Scanner input = new Scanner(System.in);
+
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("Question 2) Is it a living thing?");
+
         Scanner input2 = new Scanner(System.in);
 
         if(input.equals("outside") && input2.equals("alive")){
-            System.out.println("");
+            System.out.println("Then what else could you be thinking of besides a python?!?\n");
         }
 
 
