35135200d31e6b1d94e012ff4efeb7330777d8f2
Frank Uzoka <Frankuzoka@hotmail.com>
2020-02-16 19:44:06 -0500

    Refactor out Scanner in outside

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -25,13 +25,6 @@ public class App {
         doLogicForBoth();
     }
 
-
-    public static void askQuestion(){
-
-
-    }
-
-
     public static void doLogicForInside(){
         Scanner scan = new Scanner(System.in);
         System.out.println("Question 2) Is it a living thing? yes or no?");
@@ -45,10 +38,6 @@ public class App {
             }
 
         }
-
-
-
-
 
     public static void doLogicForOutside(){
 
@@ -67,31 +56,15 @@ public class App {
 
     public static void doLogicForBoth(){
 
-        System.out.println("TWO MORE QUESTIONS, BABY!\n" +
-                "\n" +
-                "Think of something and I'll try to guess it!");
-
-        try {
-            Thread.sleep(2000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-
-        System.out.println("Question 1) Does it stay inside or outside or both?");
-
         Scanner scan = new Scanner(System.in);
-
-        String input = scan.nextLine().toLowerCase();
-
         System.out.println("Question 2) Is it a living thing? yes or no?");
-
-        String input2 = scan.nextLine().toLowerCase();
-
-        if(input.equals("both") && input2.equals("yes")){
-            System.out.println("Then what else could you be thinking of besides a python?!?\n");
+        String input = scan.nextLine().toLowerCase();
+        if(input.equals("yes")){
+            System.out.println("Then what else could you be thinking of besides a dog?!?\n");
         }
-        if(input.equals("both") && input2.equals("no")){
-            System.out.println("Then what else could you be thinking of besides a python?!?\n");
+
+        if(input.equals("no")){
+            System.out.println("Then what else could you be thinking of besides a cell phone?!?\n");
         }
 
     }
