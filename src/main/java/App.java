d9c980bc24a875f62429b0ecd96f5ec62bb524b7
Frank Uzoka <Frankuzoka@hotmail.com>
2020-02-16 19:40:46 -0500

    Refactor out Scanner in outside

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -52,31 +52,15 @@ public class App {
 
     public static void doLogicForOutside(){
 
-        System.out.println("TWO MORE QUESTIONS, BABY!\n" +
-                "\n" +
-                "Think of something and I'll try to guess it!");
-
-        try {
-            Thread.sleep(2000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-
-        System.out.println("Question 1) Does it stay inside or outside or both?");
-
         Scanner scan = new Scanner(System.in);
-
-        String input = scan.nextLine().toLowerCase();
-
         System.out.println("Question 2) Is it a living thing? yes or no?");
-
-        String input2 = scan.nextLine().toLowerCase();
-
-        if(input.equals("outside") && input2.equals("yes")){
-            System.out.println("Then what else could you be thinking of besides a python?!?\n");
+        String input = scan.nextLine().toLowerCase();
+        if(input.equals("yes")){
+            System.out.println("Then what else could you be thinking of besides a bison?!?\n");
         }
-        if(input.equals("outside") && input2.equals("no")){
-            System.out.println("Then what else could you be thinking of besides a python?!?\n");
+
+        if(input.equals("no")){
+            System.out.println("Then what else could you be thinking of besides a \tshower curtain?!?\n");
         }
 
     }
